7782eb6a3c791786a838d47509dba16d5b1d262c
Pamela Castillo <pamela@marketpublique.com>
2012-10-06 16:08:22 -0400

    new postgres app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrcrave::Application.config.secret_token = 'c8de92a9b077154ce16e669eb21ca35213cd9e6f8c28d29dec172c0f86e54b43721e6b3ed9f1ccb0b3baed72006a793e2e74c402f9207c3ba3d9ba183af8fa09'
