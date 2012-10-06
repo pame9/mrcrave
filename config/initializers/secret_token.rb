d39c36a1479788844c622d20e9cc3b557d678029
Pamela Castillo <pamela@marketpublique.com>
2012-10-06 14:56:01 -0400

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrcrave::Application.config.secret_token = '6f5e3d166c282d8d89a92a2b6c26b16a689c1185de76b36ad6a066fccba1eda6837455df978cb2054d68d54111af87d1dcd65edc94d7e6cb5c30ab97bb288d30'
