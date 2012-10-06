2b411622636029d3b82cbb89da681c47b3ca290f
Pamela Castillo <pamela@marketpublique.com>
2012-10-06 16:29:31 -0400

    fix merge conflicts

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-<<<<<<< HEAD
+
 Mrcrave::Application.config.secret_token = 'c8de92a9b077154ce16e669eb21ca35213cd9e6f8c28d29dec172c0f86e54b43721e6b3ed9f1ccb0b3baed72006a793e2e74c402f9207c3ba3d9ba183af8fa09'
-=======
-Mrcrave::Application.config.secret_token = '6f5e3d166c282d8d89a92a2b6c26b16a689c1185de76b36ad6a066fccba1eda6837455df978cb2054d68d54111af87d1dcd65edc94d7e6cb5c30ab97bb288d30'
->>>>>>> 7d80b1e71cbb8541c9fdfa579f0aeb78a1e73851
+
+
+# Mrcrave::Application.config.secret_token = '6f5e3d166c282d8d89a92a2b6c26b16a689c1185de76b36ad6a066fccba1eda6837455df978cb2054d68d54111af87d1dcd65edc94d7e6cb5c30ab97bb288d30'
+# >>>>>>> 7d80b1e71cbb8541c9fdfa579f0aeb78a1e73851
